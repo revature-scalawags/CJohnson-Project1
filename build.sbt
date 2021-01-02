3da00df353576d9b8106a9bd05a1d52d928562ed
Chris Johnson <cjohn281@outlook.com>
2021-01-02 14:01:08 -0500

    First commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "CJohnson-Project1"
+organization := "com.revature"
+version := "1.0"
+scalaVersion := "2.13.4"
+
+libraryDependencies += "org.apache.hadoop" % "hadoop-mapreduce-client-core" % "3.2.1"
+libraryDependencies += "org.apache.hadoop" % "hadoop-common" % "3.2.1"
---
+++ src/main/scala/song/Song.scala
@@ -0,0 +1,23 @@
+package song
+
+object Song {
+  val VALENCE = 0
+  val YEAR = 1
+  val ACOUSTICNESS = 2
+  val ARTISTS = 3
+  val DANCEABILITY = 4
+  val DURATION = 5
+  val ENERGY = 6
+  val EXPLICIT = 7
+  val ID = 8
+  val INSTRUMENTALNESS = 9
+  val KEY = 10
+  val LIVENESS = 11
+  val LOUDNESS = 12
+  val MODE = 13
+  val NAME = 14
+  val POPULARITY = 15
+  val RELEASE_DATE = 16
+  val SPEECHINESS = 17
+  val TEMPO = 18
+}
